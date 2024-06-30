ddb3cf17d05bbd3707ead93300ea28f902ac5f40
Amos Oluoch <aoluoch271@gmail.com>
2024-06-30 21:25:49 +0300

    distance

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+// Code your solution in this file! 
+function distanceFromHqInBlocks(distance) {
+    const hq = 42;
+    return Math.abs(distance - hq);
+}
+
+console.log(distanceFromHqInBlocks(50))
+
+function distanceFromHqInFeet(distance) {
+    const blocks = distanceFromHqInBlocks(distance),
+    feetPerblock = 264;
+
+    return blocks * feetPerblock;
+    
+  }
+
+  console.log(distanceFromHqInFeet(50))
+
+  function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    const blocks = Math.abs(destination - start),
+    feetPerblock = 264,
+    distanceFeet = blocks * feetPerblock;
+
+    return distanceFeet;
+  }
+
+  console.log(distanceTravelledInFeet(50, 42));
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+  }
